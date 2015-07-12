c50aa7349db25d418ca008992688751ca51c37b5
senthilprasad.c <senthilprasad.c@IYC6ITDT11.iyc.ishafoundation.org>
2015-07-12 14:31:24 +0530

    addedd pms constant file

---
+++ src/main/java/org/isha/util/pms/indent/PmsIndentConst.java
@@ -0,0 +1,7 @@
+package org.isha.util.pms.indent;
+
+public class PmsIndentConst {
+	public static String Pmsindenturl="http://192.168.138.253/purchase/prf.html";
+	
+
+}
--- src/test/java/org/isha/test/pms/indent/ValidateDefaultDate.java
+++ src/test/java/org/isha/test/pms/indent/ValidateDefaultDate.java
@@ -1,8 +1,22 @@
 package org.isha.test.pms.indent;
 
 import org.isha.pariksha.common.Grid;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+import org.isha.pariksha.common.ElementUtils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import org.testng.annotations.AfterClass;
+
+//import org.isha.test.pms.indent.*;
+import org.isha.util.pms.indent.PmsIndentConst;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class ValidateDefaultDate {
 
@@ -11,7 +25,40 @@ public class ValidateDefaultDate {
 	@Test
 	public void verifyDefaultDate(){
 		
-		driver=Grid.getDriver();
+		//driver=Grid.getDriver();
+		//driver=Grid.getDriver();
+				WebDriver driver = new FirefoxDriver();
+				driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+				
+				driver.get(PmsIndentConst.Pmsindenturl);
+				//String dateval=ElementUtils.getElement(By.xpath(xpathExpression), (RemoteWebDriver)driver).getText();
+				//String dateval=driver.findElement(By.id("datepicker")
+				//System.out.println(dateval);
+				
+				
+				 Date todaydate = new Date( );
+				 
+				 SimpleDateFormat dateFrm= new SimpleDateFormat("yyyy/mm/dd");
+				 
+				 String systemdate = dateFrm.format(todaydate);
+				 System.out.println(systemdate);
+				 
+				String xpath="//label[conatins(text(),'Date:"+systemdate+"')]";
+				String dateval=ElementUtils.getElement(By.xpath(xpath), (RemoteWebDriver)driver).getText();
+		
+				System.out.println("Today date is"+dateval );
+			
+				 
+				 
+				 
+				
+			}
+			
+			@AfterClass
+			public void tearDown(){
+//				driver.quit();
+//				driver= null;
+			}
 		
 	}
-}
+
